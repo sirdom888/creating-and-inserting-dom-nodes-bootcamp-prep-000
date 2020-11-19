39bf1f50c8d1e4f692ce45eb7b3ebc427c7d91da
Dom Siravo <domcode88@gmail.com>
2020-11-19 03:45:43 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,4 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
+element.innerHTML = 'Hello, DOM!';
+element.style.backgroundColor = '#f9f9f9';
